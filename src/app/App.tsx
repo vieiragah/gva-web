fd05b73c14c053c54e299459b6bef3a12a4d6733
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-17 00:55:09 -0300

    feat: create config app

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "../shared";
+import { Outlet } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Outlet />
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
