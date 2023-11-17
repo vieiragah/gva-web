0f5880e0f62172bdc7f5c5d072c0de7be8df27d2
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-17 00:53:02 -0300

    feat: export router

---
+++ src/shared/routes/interfaces.tsx
@@ -0,0 +1,19 @@
+// const scale = "/scale";
+// const admin = "/admin";
+
+// const AUTH = {
+//   LOGIN : '/'
+// }
+// const SCALE = {
+//   DASHBOARD: `${scale}/dashboard`,
+// };
+
+// const ADMIN = {
+//   USERS: `${admin}/users`,
+// };
+
+// export const Routes = {
+//   scale: SCALE,
+//   admin: ADMIN,
+//   auth: AUTH
+// };
