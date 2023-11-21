9b9d41476a5b7cb262a870cbe4ea9fb4e3432aa8
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-21 18:52:06 -0300

    fix: altered name of component

--- src/app/home/page/Home.tsx
+++ src/app/home/page/Home.tsx
@@ -1,9 +1,9 @@
-import { HomeView, Carousel, Accordions } from "..";
+import { HomeView, CarouselComponent, Accordions } from "..";
 export const Home = () => {
   return (
     <HomeView
       key={0}
-      carousel={<Carousel key={1} />}
+      carousel={<CarouselComponent key={1} />}
       accordion={<Accordions key={2} />}
     />
   );
