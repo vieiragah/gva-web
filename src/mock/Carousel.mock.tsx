b33b8efd1a46a0fc517624a0782a416a9b53760a
Gabriel Apolinario <vieira.gah@outlook.com>
2023-11-21 18:51:19 -0300

    feat: create mock carousel

---
+++ src/mock/Carousel.mock.tsx
@@ -0,0 +1,18 @@
+export const MockCarousel = [ 
+    {
+    id: '1',
+    img: 'https://aherj.com.br/wp-content/uploads/2019/05/hospitais-2zz2owzprjbz02w220c6by6r4tbo0dcg7okv2mp31f4nm9rf0.jpg',
+    title: 'Faixada de um hospital'
+},
+{
+    id: '2',
+    img: 'https://conteudo.imguol.com.br/c/noticias/4d/2020/05/31/20052020---ala-voltada-para-pacientes-com-coronavirus-em-unidade-de-terapia-intensiva-uti-no-hospital-gilberto-novaes-em-manaus-am-1590951570416_v2_900x506.jpg',
+    title: 'UTI COVID'
+},
+{
+    id: '3',
+    img: 'https://www.be3.co/wp-content/uploads/2020/03/original-fb3eb7702f3f1ce977784f4141a9af2f.jpeg',
+    title: 'informativo'
+},
+
+]
