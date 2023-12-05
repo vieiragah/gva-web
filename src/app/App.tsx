54b40b1ae3c60a9baf41b957efff6b399745eb89
Gabriel Apolinario <vieira.gah@outlook.com>
2023-12-05 00:13:48 -0300

    fix: create restrict router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,22 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../shared/components";
 import { Box } from "@mui/material";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 function App() {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (!user) {
+      navigate("/");
+    }
+  }, [navigate]);
   return (
-    <Box margin='10px 0 70px 0'>
+    <Box margin="10px 0 70px 0">
       <Outlet />
-      {pathname != '/' ? <Sidebar/> : null}
+      {pathname != "/" ? <Sidebar /> : null}
     </Box>
   );
 }
