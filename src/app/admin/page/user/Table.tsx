6e95033d719f97522eba17038c7774e2d9d87f08
Gabriel Apolinario <vieira.gah@outlook.com>
2023-12-08 21:55:34 -0300

    feat: create table user for edited user

---
+++ src/app/admin/page/user/Table.tsx
@@ -0,0 +1,38 @@
+import {
+    TableBody,
+    TableContainer,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+  } from "@mui/material";
+import { HttpClient } from "../../../../shared";
+  //import axios from "axios";
+export const TableUser = () => {
+
+
+  return (
+    <TableContainer sx={{ margin: "80px 0 30px 0" }}>
+      <Table sx={{ marginBottom: "20px" }}>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" sx={{ fontWeight: "bolder" }}>
+              Nome
+            </TableCell>
+            <TableCell align="center" sx={{ fontWeight: "bolder" }}>
+              Setor
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow>
+              <TableCell align="center">sdafjn</TableCell>
+              <TableCell align="center">
+                asdasdas
+              </TableCell>
+            </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
