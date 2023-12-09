520fd711317d901a23043683c2598106bb1d8fa8
Gabriel Apolinario <vieira.gah@outlook.com>
2023-12-08 21:57:09 -0300

    fix: added AutProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../shared/components";
 import { Box } from "@mui/material";
-import { useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { AuthProvider } from "../shared/contexts/authentication";
 function App() {
   const { pathname } = useLocation();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const user = localStorage.getItem("user");
-    if (!user) {
-      navigate("/");
-    }
-  }, [navigate]);
   return (
-    <Box margin="10px 0 70px 0">
-      <Outlet />
-      {pathname != "/" ? <Sidebar /> : null}
-    </Box>
+    <AuthProvider>
+      <Box margin="10px 0 70px 0">
+        <Outlet />
+        {pathname != "/" ? <Sidebar /> : null}
+      </Box>
+    </AuthProvider>
   );
 }
 
